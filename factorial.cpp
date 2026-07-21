b8aa71414cba93d964f90cf4ef708b3400a7f541
rajvasagadekar <rajvasagadekar@gmail.com>
2026-07-21 09:43:35 +0000

    success

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, fact=1;
+    cin>>num;
+    for (int i=1;i<=num;i++) {
+        fact=fact*i;
+    }
+    cout<<"factorial="<<fact;
+
+    return 0;
+}
