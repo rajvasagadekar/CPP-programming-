d77b6ea8b52e02e2b23452359547da633104456f
rajvasagadekar <rajvasagadekar@gmail.com>
2026-07-21 09:20:50 +0000

    success

---
+++ positivenegative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=20;
+    if(n>0){
+        cout<<"number is positive";
+    }
+    else if(n<0){
+        cout<<"number is negaative";
+    }
+    else{
+        cout<<"number is zero";
+    }
+    return 0;
+}
