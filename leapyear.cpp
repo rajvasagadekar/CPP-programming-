2861fcf722174161995f0f2b1fafaa887e31b6dd
rajvasagadekar <rajvasagadekar@gmail.com>
2026-07-21 09:27:35 +0000

    success

---
+++ leapyear.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int year=2024;
+    if(year%4==0){
+        cout<<"Leap Year";
+    }
+    else{
+        cout<<"Not a leap year";
+    }
+    return 0;
+}
