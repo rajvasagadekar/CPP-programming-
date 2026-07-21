26d41f73868b04c465e6cf4003cac90af9869622
rajvasagadekar <rajvasagadekar@gmail.com>
2026-07-21 09:05:42 +0000

    success

---
+++ largeof2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=12,b=20;
+    if(a>b){
+        cout<<"a is greater";
+    }
+    else if (b>a){
+        cout<<"b is greater";
+    }
+    else {
+        cout<<"same number";
+    }
+}
