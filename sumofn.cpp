0579cef6ad3359c7f9d78f461dd1116f1f1373bb
rajvasagadekar <rajvasagadekar@gmail.com>
2026-07-28 09:04:01 +0000

    success

---
+++ sumofn.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum = 0;
+
+    cout<<"enter a number :";
+    cin>>n;
+
+    for (int i = 1;i <= n; i++){
+
+        sum+=i;
+    }
+
+    cout<<"sum of first" <<n<< "number is ="<<sum;
+
+    return 0;
+}
