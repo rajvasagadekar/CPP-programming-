391e0538775e96bf9c45c2ed7b969e3977403f32
rajvasagadekar <rajvasagadekar@gmail.com>
2026-09-22 08:50:00 +0000

    success

--- Banking.cpp
+++ Banking.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
 class Savings {
     string name;
     int accNo;
@@ -10,17 +15,95 @@ public:
         balance = b;
     }
 
-void deposit (double amount){
-    balance = balance + amount;
-    cout<< "Deposited : Rs." << amount << endl;
-}
-
-void withdraw(double amount){
-    if (anount <= balance) {
-        balance=balance-amount;
-        cout<<"withdraw: Rs."<< amount << endl;
-    }else{
-        cout<< "Insufficient Balance!" << endl;
+    void deposit(double amount) {
+        balance = balance + amount;
+        cout << "Deposited: Rs. " << amount << endl;
     }
-}
 
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance = balance - amount;
+            cout << "Withdrawn: Rs. " << amount << endl;
+        } else {
+            cout << "Insufficient Balance!" << endl;
+        }
+    }
+
+    void addInterest(double rate) {
+        double interest = balance * rate / 100;
+        balance = balance + interest;
+
+        cout << "Interest Added: Rs. " << interest << endl;
+    }
+
+    void show() {
+        cout << "\n--- Savings Account ---" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Account No: " << accNo << endl;
+        cout << "Balance: Rs. " << balance << endl;
+    }
+};
+
+
+class Checking {
+    string name;
+    int accNo;
+    double balance;
+
+public:
+    Checking(string n, int a, double b) {
+        name = n;
+        accNo = a;
+        balance = b;
+    }
+
+    void deposit(double amount) {
+        balance += amount;
+        cout << "Deposited: Rs. " << amount << endl;
+    }
+
+    void withdraw(double amount, double fee) {
+        double total = amount + fee;
+
+        if (total <= balance) {
+            balance -= total;
+            cout << "Withdrawn: Rs. " << amount << endl;
+            cout << "Transaction Fee: Rs. " << fee << endl;
+        } else {
+            cout << "Insufficient Balance!" << endl;
+        }
+    }
+
+    void show() {
+        cout << "\n--- Checking Account ---" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Account No: " << accNo << endl;
+        cout << "Balance: Rs. " << balance << endl;
+    }
+};
+
+
+int main() {
+
+    Savings s("Raj", 101, 5000);
+    Checking c("Amit", 102, 3000);
+
+    s.show();
+
+    s.deposit(1000);
+    s.withdraw(2000);
+    s.addInterest(3);
+
+    s.show();
+
+
+   
+    c.show();
+
+    c.deposit(1500);
+    c.withdraw(1000, 20);
+
+    c.show();
+
+    return 0;
+}
